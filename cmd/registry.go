18d8b54714ffc56c7eb8ed693a173ce4f9e72182
ankit <ankitgci1@gmail.com>
2023-12-01 13:45:31 +0530

    gm rollup work with external celestia node

--- cmd/registry.go
+++ cmd/registry.go
@@ -7,5 +7,5 @@ var CommanderRegistry = map[string]cli.NodeCommander{
 	"celestia-light":  cli.NewCelestiaLightCommander(),
 	"celestia-bridge": cli.NewCelestiaBridgeCommander(),
 	"avail-light":     cli.NewAvailLightCommander(),
-	"gmd":     cli.NewGmdCommander(),
+	"gmd-rollup":     cli.NewGmdCommander(),
 }
