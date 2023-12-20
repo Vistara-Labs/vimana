131dfd933d1e1435e1eb285c51d9c492f59dafef
ankit <ankitgci1@gmail.com>
2023-12-20 12:52:38 +0530

    da script update

--- scripts/rollup_init.sh
+++ scripts/rollup_init.sh
@@ -57,5 +57,5 @@ sudo rm -rf "/tmp/vimcel"
 
 # init light node
 # start light node
-celestia light init --p2p.network mocha
+# celestia light init --p2p.network mocha
 celestia light start --core.ip rpc-mocha.pops.one --p2p.network mocha
