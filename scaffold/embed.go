59c0a8081b378e376afef401a198654b690a3066
Mayur Chougule <mayurchougule1@gmail.com>
2024-06-15 01:27:36 -0400

    cleanup

--- scaffold/embed.go
+++ scaffold/embed.go
@@ -5,17 +5,5 @@ import (
 	_ "embed"
 )
 
-//go:embed templates/grpc.gotmpl
-var templateGrpcGoTemplate embed.FS
-
 //go:embed templates/*.*tmpl
 var createTemplateFS embed.FS
-
-////go:embed templates/resource_scaffold.gotmpl
-// var resourceScaffoldGoTemplate string
-
-// //go:embed templates/data_source_scaffold.gotmpl
-// var dataSourceScaffoldGoTemplate string
-
-////go:embed templates/provider_scaffold.gotmpl
-// var providerScaffoldGoTemplate string
